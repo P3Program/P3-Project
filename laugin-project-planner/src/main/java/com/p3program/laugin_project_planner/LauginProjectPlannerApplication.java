b2c1705c0fa7efde66ac6fcc45340b83f9f8e3a1
MKeinicke <mkeini24@student.aau.dk>
2025-10-01 10:56:32 +0200

    Hello first try at spring-boot initialization

---
+++ laugin-project-planner/src/main/java/com/p3program/laugin_project_planner/LauginProjectPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.p3program.laugin_project_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauginProjectPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauginProjectPlannerApplication.class, args);
+	}
+
+}
