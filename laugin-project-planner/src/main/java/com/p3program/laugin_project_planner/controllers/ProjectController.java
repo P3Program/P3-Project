5cbc1353d56d2a93748e80b5b17d032f5276971b
HedinnSA <harnar24@student.aau.dk>
2025-10-15 11:30:40 +0200

    Added Hello World and a method to findAll from DB

--- laugin-project-planner/src/main/java/com/p3program/laugin_project_planner/controllers/ProjectController.java
+++ laugin-project-planner/src/main/java/com/p3program/laugin_project_planner/controllers/ProjectController.java
@@ -1,9 +1,24 @@
 package com.p3program.laugin_project_planner.controllers;
 
+import com.p3program.laugin_project_planner.projects.Project;
+import com.p3program.laugin_project_planner.repositories.ProjectRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping("/show")
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
 }
---
+++ laugin-project-planner/src/main/java/com/p3program/laugin_project_planner/repositories/ProjectRepository.java
@@ -0,0 +1,7 @@
+package com.p3program.laugin_project_planner.repositories;
+
+import com.p3program.laugin_project_planner.projects.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
